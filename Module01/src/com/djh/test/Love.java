0afdd4bc4bcdca67f43a67c3b6e6abe635a564f3
djh-orphan <1370317019@qq.com>
2021-07-04 15:13:59 +0800

    change name

---
+++ Module01/src/com/djh/test/Love.java
@@ -0,0 +1,18 @@
+package com.djh.test;
+
+public class Love {
+    public static void main(String[] args) {
+//      System.out.println("Helloworld");
+//        System.out.println("dlsfofowefqwenfiwddna");
+        System.out.print("\t*\t\t\t\t\t\t\t\t*\t\n*\t\t*\t\t" +
+                "I Love java\t\t*\t\t*\n" +
+                "\t*\t\t\t\t\t\t\t\t*\t\n" +
+                "\t  *\t\t\t\t\t\t\t  *\t\n" +
+                "\t    *\t\t\t\t\t\t*\t\n" +
+                "\t      *\t\t\t\t\t  *\t\n" +
+                "\t        *\t\t\t\t*\t\n" +
+                "\t          *\t\t\t  *\t\n" +
+                "\t            *\t\t*\t\n" +
+                "\t              \t*\t\n");
+    }
+}
