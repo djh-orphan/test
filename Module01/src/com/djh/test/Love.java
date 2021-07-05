6a11b0fcd6adf57634051ba1117f5a123ea58ef0
djh-orphan <1370317019@qq.com>
2021-07-05 17:51:44 +0800

    拆分

--- Module01/src/com/djh/test/Love.java
+++ Module01/src/com/djh/test/Love.java
@@ -1,9 +1,10 @@
 package com.djh.test;
 
+
 public class Love {
     public static void main(String[] args) {
-//      System.out.println("Helloworld");
-//        System.out.println("dlsfofowefqwenfiwddna");
+// heart
+
         System.out.print("\t*\t\t\t\t\t\t\t\t*\t\n*\t\t*\t\t" +
                 "I Love java\t\t*\t\t*\n" +
                 "\t*\t\t\t\t\t\t\t\t*\t\n" +
@@ -14,5 +15,102 @@ public class Love {
                 "\t          *\t\t\t  *\t\n" +
                 "\t            *\t\t*\t\n" +
                 "\t              \t*\t\n");
+
+
+//        long a=1221323222220222222L;
+//        char a = 65;
+//        byte b = 12;
+//        byte c = 10;
+//        c++;
+//        byte d = (byte)(b + c);
+//        System.out.println(getType(c));
+//        System.out.println(c);
+//        System.out.println("5+5="+5+5);
+
+
+//自加自减测试
+        int i1 = 10;
+        int i2 = 20;
+        int i = i1++;
+        System.out.println("i=" + i);
+        System.out.println("i1=" + i1);
+        i = ++i1;
+        System.out.println("i=" + i);
+        System.out.println("i1=" + i1);
+        i = i2--;
+        System.out.println("i=" + i);
+        System.out.println("i2=" + i2);
+        i = --i2;
+        System.out.println("i=" + i);
+        System.out.println("i2=" + i2);
+
+//打印数字
+        int num;
+        int n;
+        n = num = 234;
+//        n,num=23,34; erro
+        num += 1;
+
+        int num1 = num / 100;
+
+        int num2 = num % 100 / 10;
+        int num3 = num % 10;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+
+//        int x = 1;
+//        int y = 1;
+        boolean x = true;
+        boolean y = false;
+        short z = 42;
+        //if(y == true)
+        if ((z++ == 42) & (y = true)) {
+            z++;
+            System.out.println(y);
+        }
+//        if ((x = false) || (++z == 45)) z++;
+
+        int n1 = 100;
+        int n2 = 1223;
+        int n3 = 123;
+        int max = (n1 >= n2) ? ((n1 >= n3) ? n1 : n3) : ((n2 >= n3) ? n2 : n3);
+        System.out.println("z=" + max);
+        boolean b = true;
+//如果写成if(b=false)可以通过，b=false 给b赋值的同时返回false
+        if (b = false)
+            System.out.println("a");
+        else if (b)
+            System.out.println("b");
+        else if (!b)
+            System.out.println("c");
+        else
+            System.out.println("d");
+
+
+//        Scanner sc=new Scanner(System.in);
+//        int ww=sc.nextInt();
+//        System.out.println(ww);
+
+        int as = 10;
+        switch (as) {
+            case 9:
+                System.out.println(as);
+                break;
+            case 10:
+                System.out.println(3 + 4 + "as");
+                break;
+            default:
+                break;
+        }
+
+
     }
+
+    public static String getType(Object o) { //获取变量类型方法
+
+        return o.getClass().toString(); //使用int类型的getClass()方法
+
+    }
+
 }
