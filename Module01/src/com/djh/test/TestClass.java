d1fbbc958e167d1d0b0528311b219d3bfc507f11
djh-orphan <1370317019@qq.com>
2021-07-09 09:50:38 +0800

    2021.7.9 学习冒泡排序和快速排序 并测试。 在随机性较好的情况下，快速排序具有较好的优越性。 下一步将研究快速排序的非递归实现以及java的后续学习。

--- Module01/src/com/djh/test/TestClass.java
+++ Module01/src/com/djh/test/TestClass.java
@@ -1,6 +1,8 @@
 package com.djh.test;
 
 
+import java.util.Scanner;
+
 public class TestClass {
     public static void main(String[] args) {
         //测试一
@@ -16,43 +18,54 @@ public class TestClass {
 
 
         //测试二
-        Student stu[] = new Student[20];
-        for (int i = 0; i < 20; i++) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("请输入尺寸大小:\t");
+        int size = sc.nextInt();
+
+        Student stu[] = new Student[size];
+        for (int i = 0; i < size; i++) {
             stu[i] = new Student();
             stu[i].ID = i + 1;
-            stu[i].score = (int) Math.round(Math.random() * 100 % 101);
+            /**
+             * 生成指定区间整数随机数的办法 [min,max]
+             * 1）不是从0开始
+             * 使用(int)(Math.raandom()*max%(max-min+1)+min)
+             * 2)从0开始
+             * 使用(int) Math.round(Math.random()*max%(max-min+1)+min)
+             */
+            stu[i].score = (int) Math.round(Math.random() * 750 % 751);
             stu[i].state = (int) (Math.random() * 9 % 9 + 1);
-//            System.out.print(stu[i].ID+":"+stu[i].score+"\t");
-//            if (stu[i].state == 3)
-//                System.out.println(stu[i].ID + "\t");
+
         }
 
-        //冒泡排序
-//        Student st;
-//        for (int i = 0; i < stu.length; i++) {
-//            for (int j = 0; j < stu.length - 1 - i; j++) {
-//                if (stu[j].score < stu[j + 1].score) {
-//                    st = stu[j];
-//                    stu[j] = stu[j + 1];
-//                    stu[j + 1] = st;
-//                }
-//            }
+        long sta, end;
+        Student stu1[] = new Student[stu.length];
+        System.arraycopy(stu, 0, stu1, 0, stu.length);
+
+
+//        冒泡排序
+        sta = System.nanoTime();
+        bubbleSort(stu);
+        end = System.nanoTime();
+        System.out.println(end - sta);
+        System.out.println("冒泡排序的时间" + (double) (end - sta) + "ns");
+
+//        快速排序
+
+        sta = System.nanoTime();
+        quickSort(stu1, 0, stu.length - 1);
+        end = System.nanoTime();
+        System.out.println(end - sta);
+        System.out.println("快速排序的时间" + (double) (end - sta) + "ns");
+
+
+//        for (int i = 0; i < size; i++) {
+//            System.out.println(i + 1 + ":" + stu1[i].ID + ":" + stu1[i].score);
 //        }
-//        bubbleSort(stu);
-        //快速排序
-        quickSort(stu, 0, stu.length - 1);
-        for (int i = 0; i < 20; i++) {
-            System.out.println(i + 1 + ":" + stu[i].ID + ":" + stu[i].score);
-        }
     }
 
-    /**
-     * @param stu
-     * @return void
-     * 冒泡排序的实现
-     * @author duan
-     * @date 2021-07-08 21:07
-     */
+
     public static void bubbleSort(Student[] stu) {
         Student st;
         for (int i = 0; i < stu.length; i++) {
@@ -66,34 +79,27 @@ public class TestClass {
         }
     }
 
-    /**
-     * @param stu
-     * @param leftIndex
-     * @param rightIndex
-     * @return void
-     * 快速排序
-     * @author duan
-     * @date 2021-07-08 21:07
-     */
+
     public static void quickSort(Student[] stu, int leftIndex, int rightIndex) {
         if (leftIndex >= rightIndex)
             return;
         Student st;
-
+        //注意：leftIndex和rightIndex是要排序的起点和终点，不可以直接拿来使用，需要单独赋值后使用
         int left = leftIndex;
         int right = rightIndex;
         st = stu[left];
         while (left < right) {
-            while (left < right && stu[right].score > st.score) {
+            while (left < right && stu[right].score >= st.score) {
                 right--;
             }
             stu[left] = stu[right];
-            while (left < right && stu[left].score < st.score) {
+            while (left < right && stu[left].score <= st.score) {
                 left++;
             }
             stu[right] = stu[left];
         }
         stu[left] = st;
+        //注意：迭代过程中left和right会改变，需要对left进行存储
         int temp = left;
         quickSort(stu, leftIndex, temp - 1);
         quickSort(stu, temp + 1, rightIndex);
