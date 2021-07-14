07268a449d207512f640908265f5f377509f1e51
djh-orphan <1370317019@qq.com>
2021-07-14 20:25:09 +0800

    2021.7.13 已实现：（全部完成） 1./login 全部完成 2./quit 全部完成 3.xxxxx 广播发送 全部完成 4./to usr xxxxx 全部完成 5.//hi usr_name 完成 6. /who 完成 7. /history 完成 8. 解决不输入直接Enter的bug 9.解决checkonline问题 10.实现/smile TODO：

--- socket/src/com/djh/client/ClientTest.java
+++ socket/src/com/djh/client/ClientTest.java
@@ -8,7 +8,7 @@ import java.io.IOException;
  */
 public class ClientTest {
     public static void main(String[] args) throws IOException {
-        Client client = new Client("127.0.0.4");
+        Client client = new Client("127.0.0.6");
         client.login();
         if (client.isLogin)
             client.talk();
